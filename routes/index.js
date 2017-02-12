2d985d0d3b2e301fe767b2d9b4b08fd1013dd2a2
Matt Seaton <matt.r.seaton@gmail.com>
2017-02-12 00:43:45 -0700

    did I forget anything last time?

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@
 var express = require('express');
 var router = express.Router();
 const knex = require('../db/knex');
-const dotenv = require('dotenv').config();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
