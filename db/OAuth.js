e56cf35347f68c559de9147b4ca7f9fb64391080
Tyler Briskie <tylerbriskie@gmail.com>
2017-02-13 10:05:52 -0700

    added some wager routes and beginnings of oauth

---
+++ db/OAuth.js
@@ -0,0 +1,13 @@
+//
+//
+// module.exports = {
+//   getAllUsers: function getAllUsers(){
+//     return knex("users")
+//     .select()
+//   },
+//   getUserByGoogleId : function getUserByGoogleId(profileID){
+//     return knex("users")
+//     .select()
+//     .where('google_id', profileID)
+//   }
+// }
